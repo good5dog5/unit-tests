4d0e8db727e42dd64d816dec47c98980102e5560
Jordan Huang <good5dog5@gmail.com>
2016-03-22 14:15:23 +0800

    Use "Here Document" to rewrite test-swap.sh in clearer way.

--- test-swap.sh
+++ test-swap.sh
@@ -1,22 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo b main >> _tmp.gdb
-echo r >> _tmp.gdb
-echo source scripts/create_list.gdb >> _tmp.gdb
-echo source scripts/printf_list.gdb >> _tmp.gdb
-echo source scripts/find_node_address.gdb >> _tmp.gdb
 
-echo 'set $head = (List*)0' >> _tmp.gdb
-echo 'set $node1 = (List*)0' >> _tmp.gdb
-echo 'set $node2 = (List*)0' >> _tmp.gdb
+gdbfile="_tmp.gdb"
 
+cat <<EOF > $gdbfile
+b main
+r
+source scripts/create_list.gdb
+source scripts/printf_list.gdb
+source scripts/find_node_address.gdb
+set \$head = (List*)0
+set \$node1 = (List*)0
+set \$node2 = (List*)0
+EOF
+
+# stdin replace by $1(data-swap.in)
 exec < $1
+
+# read first line of data-swap.in, var=4
 read var
-echo 'create_list $head' $var >> _tmp.gdb
+echo 'create_list $head' $var >> $gdbfile
 
 exec < $1
 echo ' '>>$2
-echo 'set logging file '$2 >> _tmp.gdb
+echo 'set logging file '$2 >> $gdbfile
 
 while (read var) 
 do
@@ -25,22 +32,26 @@ do
 	read var
 	node_2=$var
 
-	echo 'set logging on' >> _tmp.gdb
-	echo 'p "test begin"' >> _tmp.gdb
 
-	echo 'p "old_list"' >> _tmp.gdb
-	echo 'printf_list $head' >> _tmp.gdb
-	echo 'set logging off' >> _tmp.gdb
-	echo 'find_node_address $head '$node_1' $node1' >> _tmp.gdb
-	echo 'find_node_address $head '$node_2' $node2' >> _tmp.gdb
-	echo 'p $head = swap($head,$node1,$node2)' >> _tmp.gdb
-	echo 'set logging on' >> _tmp.gdb
-	echo 'p "change '$node_1'  '$node_2'"' >> _tmp.gdb
-	echo 'p "new_list"' >> _tmp.gdb
-	echo 'printf_list $head' >> _tmp.gdb
-	echo 'set logging off' >> _tmp.gdb
+cat >> $gdbfile << HERE
+set logging on
+p "test begin"
+p "old_list"
+printf_list \$head
+set logging off
+find_node_address \$head $node_1 \$node1
+find_node_address \$head $node_2 \$node2
+p \$head = swap(\$head,\$node1,\$node2)
+set logging on
+p "change $node_1  $node_2"
+p "new_list"
+printf_list \$head
+set logging off
+HERE
+
 done
 
+# [-q | -quiet]  Don't print the introductory and copyright message
+# [-x | -command=File] Execute GDB command from file File  
 gdb -q -x _tmp.gdb bin-swap >> /dev/null
-
-rm _tmp.gdb
+rm $gdbfile
